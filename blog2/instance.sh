b32f2f511c5bfeb063c9c71fb09f12cca3c795b8
Bikram <bikram.kumarsaw@relevancelab.com>
2018-09-03 18:20:38 +0530

    added

--- blog2/instance.sh
+++ blog2/instance.sh
@@ -1,7 +1,10 @@
+#!/bin/sh
 #Launch Instance
-aws ec2 run-instances --image-id ami-09cafebc6e2875712 --instance-type t2.micro --count 1 --security-group-ids sg-02e882e1447c40c07 --key-name blogging --profile user2
+aws ec2 run-instances --image-id ami-00b6a8a2bd28daf19 --instance-type t2.micro --count 1 --security-group-ids sg-04c953dca9dfb39ce --key-name log --profile bikramrl
 #id=$(aws ec2 describe-instances --query Reservations[*].Instances[*].InstanceId[] --profile user2 --output text)
 #echo id=$(aws ec2 describe-instances --query "ReservationId[*].Instances[*].Instanceid[]" --profile user2 --output text)
-#var=$(aws ec2 describe-instances --query Reservations[*].Instances[*].PublicIpAddress[] --profile user2 --output text)
-#echo $var
+IP=$(aws ec2 describe-instances --query Reservations[*].Instances[*].PublicIpAddress[] --profile user2 --output text)
+echo $IP1
+echo IP1=$IP >> /etc/profile
+source /etc/profile
 #echo $id 
